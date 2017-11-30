a39a1a81e2b2c180ac2d5df78b729cbd11cf7d4c
aadamcz <33779500+aadamcz@users.noreply.github.com>
2017-11-30 21:05:50 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,11 @@ Task requirements modified.
 Data is incorrect when parameters <=0 are passed. 
 */
 
-function getTriangleArea(a,h) {
-	if (a<=0 || h<=0) {
+function getTriangleArea(a, h) {
+	if (a <= 0 || h <= 0) {
 		return "Incorrect data";
 	} else {
-		return (a*h)/2;
+		return (a * h) / 2;
 	}
 }
 
@@ -17,3 +17,12 @@ var a = 0;
 var h = 2;
 
 getTriangleArea(a,h);
+
+var triangle1Area = getTriangleArea(3,6);
+//console.log(triangle1Area);
+
+var triangle2Area = getTriangleArea(4,3);
+//console.log(triangle2Area);
+
+var triangle3Area = getTriangleArea(17,8);
+//console.log(triangle3Area);
